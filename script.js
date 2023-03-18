8b9e791ba4b7e76acb8732da276c820840a25e09
ChabJose <18470225@campeche.tecnm.mx>
2023-03-18 13:54:35 -0600

    Sitio web  responsivo completo Menu para movil funcional

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toogle');
 const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
 
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle('nav-menu-visible');
+    body.classList.toggle('body-scroll');
 } )
